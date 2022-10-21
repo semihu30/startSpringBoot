c764487bb9281594133baeb101110f1b62739ec6
semih <semihu@naver.com>
2022-10-21 11:13:00 +0900

    init

---
+++ src/test/java/com/hynixExam/startSpringBoot/StartSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hynixExam.startSpringBoot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StartSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
