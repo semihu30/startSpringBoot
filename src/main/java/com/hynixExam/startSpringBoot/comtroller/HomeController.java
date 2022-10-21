14ef4ba1871f10e548fd8ee4478afa73f37f20fc
semih <semihu@naver.com>
2022-10-21 15:44:55 +0900

    layout 구성

---
+++ src/main/java/com/hynixExam/startSpringBoot/comtroller/HomeController.java
@@ -0,0 +1,12 @@
+package com.hynixExam.startSpringBoot.comtroller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+    @GetMapping
+    public String index() {
+        return "index";
+    }
+}
